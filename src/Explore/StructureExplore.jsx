0cbeac193758cadb2f1e4bea0ce1945e6c6a5a10
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-03 19:30:20 -0400

    Se agrego el componente estructura de Expore

---
+++ src/Explore/StructureExplore.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Search from '../BarraNavegacion/Components/Search'
+
+export default function StructureExplore() {
+  return (
+    <section>
+        <Search/>
+        
+    </section>
+  )
+}
