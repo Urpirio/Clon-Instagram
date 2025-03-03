fcfe402360b8ee1351c535770fe5e91227d312ac
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-03 19:32:10 -0400

    Se agrego data Story

---
+++ src/Home/Data/DataStory.js
@@ -0,0 +1,54 @@
+import CocoFoto from '../Img/CocoFoto.png';
+import CocoFotoLlora from '../Img/CocoFotoLlora.png';
+import CocoFotoMadrid from '../Img/CocoFotoMadrid.png';
+import CocoFotoMago from '../Img/CocoFotoMago.png';
+import CocoFotoNavidad from '../Img/CocoFotoNavidad.png';
+import CocoFotoNormal from '../Img/CocoFotoNormal.png';
+import CocoFotoPrince from '../Img/CocoFotoPirata.png';
+import CocoFotoRey from '../Img/CocoFotoRey.png';
+import CocoFotoXbox from '../Img/CocoFotoXbox.png';
+import CocoFotoPirata from '../Img/CocoFotoPirata.png';
+
+const DataStory = [
+    {
+        img: CocoFoto,
+        Name: 'Prueba',
+    },
+    {
+        img: CocoFotoLlora,
+        Name: 'Prueba',
+    },
+    {
+        img: CocoFotoMadrid,
+        Name: 'Prueba',
+    },
+    {
+        img: CocoFotoMago,
+        Name: 'Prueba',
+    },
+    {
+        img: CocoFotoNavidad,
+        Name: 'Prueba',
+    },
+    {
+        img: CocoFotoNormal,
+        Name: 'Prueba',
+    },
+    {
+        img: CocoFotoPrince,
+        Name: 'Prueba',
+    },
+    {
+        img: CocoFotoRey,
+        Name: 'Prueba',
+    },
+    {
+        img: CocoFotoXbox,
+        Name: 'Prueba',
+    },
+    {
+        img: CocoFotoPirata,
+        Name: 'Prueba',
+    }
+]
+export default DataStory;
