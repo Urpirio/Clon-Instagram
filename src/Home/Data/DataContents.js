059f22677cda6902a67a592d60f6c299a47ca384
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-03 19:31:58 -0400

    Se agrego data Contents

---
+++ src/Home/Data/DataContents.js
@@ -0,0 +1,64 @@
+import FotoJohanMontas from '../Img/DataContent_Img/FotoPerfil/JohanMotas.png';
+import Paisaje from '../Img/DataContent_Img/FotoPubliciones/Paisaje.png'
+
+const DataContents = [
+    {
+        FotoPerfil: FotoJohanMontas,
+        UserName: 'Johan009',
+        Hour: '6h',
+        Publicacion: Paisaje,
+        Description: 'Hermoso Atardecer',
+
+    },
+    {
+        FotoPerfil: FotoJohanMontas,
+        UserName: 'Johan009',
+        Hour: '6h',
+        Publicacion: Paisaje,
+        Description: 'Hermoso Atardecer',
+
+    },
+    {
+        FotoPerfil: FotoJohanMontas,
+        UserName: 'Johan009',
+        Hour: '6h',
+        Publicacion: Paisaje,
+        Description: 'Hermoso Atardecer',
+
+    },
+    {
+        FotoPerfil: FotoJohanMontas,
+        UserName: 'Johan009',
+        Hour: '6h',
+        Publicacion: Paisaje,
+        Description: 'Hermoso Atardecer',
+
+    },
+    
+]
+export default DataContents;
+
+// {
+    //     FotoPerfil: '',
+    //     UserName: '',
+    //     Hour: '',
+    //     Publicacion: '',
+    //     Description: '',
+
+    // },
+    // {
+    //     FotoPerfil: '',
+    //     UserName: '',
+    //     Hour: '',
+    //     Publicacion: '',
+    //     Description: '',
+
+    // },
+    // {
+    //     FotoPerfil: '',
+    //     UserName: '',
+    //     Hour: '',
+    //     Publicacion: '',
+    //     Description: '',
+
+    // }
