7641394c7624f179af3fef632a461d42075ee208
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-04 15:25:39 -0400

    Se agrego el data Explore del componente

---
+++ src/Explore/Data/DataExplore.js
@@ -0,0 +1,17 @@
+import LineGreen from '../img/LineGreen.png';
+import Nieve from '../img/Nieve.png';
+import NieveMuntain from '../img/NieveMuntain.png';
+import Paradise from '../img/Paradise.png';
+import Video1 from '../video/video1.mp4';
+
+const DataExplore = [
+    {
+        Img1: LineGreen,
+        Img2: Nieve,
+        Img3: NieveMuntain,
+        Img4: Paradise,
+        video1: Video1,
+    },
+   
+];
+export default DataExplore;
