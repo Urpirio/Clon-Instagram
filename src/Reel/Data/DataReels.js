c36ba4d0e18b71515171a639ff8513501c80ae47
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-04 15:28:54 -0400

    Se agrego el componente Data reels

---
+++ src/Reel/Data/DataReels.js
@@ -0,0 +1,50 @@
+import video1 from '../video/video1.mp4';
+import video2 from '../video/video2.mp4';
+import video3 from '../video/video3.mp4';
+import FotoPerfil1 from '../img/fotosPerfil/DogMeme.png';
+import FotoPerfil2 from '../img/fotosPerfil/goku.png';
+import FotoPerfil3 from '../img/fotosPerfil/persona.png';
+
+const DataReels = [
+    {
+        Reel: video1,
+        Fotoperfil: FotoPerfil1,
+        UserName: 'Pepe004',
+        DescriptionReel: '',
+        MusicName: 'Random music',
+        LocationPublicacion: 'Dominican Republic',
+        Likes: '10k',
+        Comments: '123',
+        id: 'video1',
+        className: 'Play1',
+
+    },
+    {
+        Reel: video2,
+        Fotoperfil: FotoPerfil2,
+        UserName: 'KAKAroto29',
+        DescriptionReel: '',
+        MusicName: 'Random music',
+        LocationPublicacion: 'Dominican Republic',
+        Likes: '10k',
+        Comments: '123',
+        id: 'Video2',
+        className: 'Play2',
+
+    },
+    {
+        Reel: video3,
+        Fotoperfil: FotoPerfil3,
+        UserName: 'ElIndio24',
+        DescriptionReel: '',
+        MusicName: 'Random music',
+        LocationPublicacion: 'Dominican Republic',
+        Likes: '10k',
+        Comments: '123',
+        id: 'Video3',
+        className: 'Play3',
+
+    }
+
+];
+export default DataReels;
