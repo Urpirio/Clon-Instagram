9065ee85f26e3ec873db2dfd6669594afbfb15ae
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-04 15:28:00 -0400

    Se agrego el componente mensaje

---
+++ src/Message/components/Message.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Style/Message.less';
+
+export default function Message() {
+    
+
+
+  return (
+    <section className='ContainerMessage V4'>
+        <div>
+
+        </div>
+    </section>
+  )
+}
