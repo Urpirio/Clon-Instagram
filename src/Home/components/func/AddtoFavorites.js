89387166008216fd0710b023056a16baafce49da
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-06 13:55:30 -0400

    Se creo una funcion externa Add to favorite

---
+++ src/Home/components/func/AddtoFavorites.js
@@ -0,0 +1,28 @@
+// import ModalThree from "./ModalThree";
+import ModalThree from "./Modalthree";
+import DataContents from "../../Data/DataContents";
+import { UserClick } from "./Modalthree";
+
+const Add_to_Favorites = () => {
+    const LargoArray = DataContents.length;
+    for(let x = 0 ; x < LargoArray; x++){
+        if(UserClick ==  DataContents[x].Number){
+           if(DataContents[x].BoleanoF != true){
+                DataContents[x].BoleanoF = true;
+                let Icon = document.createElement('i');
+                Icon.className = 'bx bxs-star';
+                let DC = document.querySelector(`.${DataContents[x].classNameF}`);
+                DC.append(Icon);
+            
+           }else{
+                DataContents[x].BoleanoF = false;
+                let DC = document.querySelector(`.${DataContents[x].classNameF}`);
+                DC.innerHTML = '';
+           }
+        }else{
+            continue;
+        }
+    }
+    ModalThree()
+};
+export default Add_to_Favorites;
