4a2f4c10b44615676da5e88dd906168418962bda
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-05 20:45:01 -0400

    Se exporto el modal three al componente.

--- src/Structure.jsx
+++ src/Structure.jsx
@@ -6,12 +6,14 @@ import './Structure.less';
 import Explore from "./Explore/components/Explore";
 import Reels from "./Reel/components/Reels";
 import Message from "./Message/components/Message";
+import Modal_threePoint from "./Home/components/Modal_threePoint";
 
 
 export default function Structure() {
   return (
     <section className="Structure">
         <Search/>
+        <Modal_threePoint/>
         <HomeStructure/>
         <Explore/>
         <Reels/>
