35c0b17c7179e5ec71114cd20286719f060a1f6e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-06 14:00:04 -0400

    Se importo mas contenido y se agregaron mas propiedades a los objetos

--- src/Home/Data/DataContents.js
+++ src/Home/Data/DataContents.js
@@ -1,41 +1,60 @@
-import FotoJohanMontas from '../Img/DataContent_Img/FotoPerfil/JohanMotas.png';
+import ChicoNike from '../Img/DataContent_Img/FotoPerfil/ChicoNike.png';
+import HombreR from '../Img/DataContent_Img/FotoPerfil/HombreR.png';
+import PixarAnimation from '../Img/DataContent_Img/FotoPerfil/PixarAnimation.png';
+import SpiderMan  from '../Img/DataContent_Img/FotoPerfil/Spiderman.png';
+
 import Paisaje from '../Img/DataContent_Img/FotoPubliciones/Paisaje.png'
+import NoWayHome from '../Img/DataContent_Img/FotoPubliciones/NowayHome.png';
+import Codejs from '../Img/DataContent_Img/FotoPubliciones/CodesJs.png';
+import GATO from '../Img/DataContent_Img/FotoPubliciones/GATO.png';
 
 const DataContents = [
     {
-        FotoPerfil: FotoJohanMontas,
+        FotoPerfil: ChicoNike,
         UserName: 'Johan009',
         Hour: '6h',
-        Publicacion: Paisaje,
-        Description: 'Hermoso Atardecer',
+        Publicacion: Codejs,
+        Description: 'Alfin aprendi java scripts',
         Number: '1',
+        className: 'Johan009',
+        classNameF: 'Johan009F',
+        BoleanoF: false,
 
     },
     {
-        FotoPerfil: FotoJohanMontas,
+        FotoPerfil: HombreR,
         UserName: 'JavierN',
         Hour: '6h',
         Publicacion: Paisaje,
         Description: 'Hermoso Atardecer',
         Number: '2',
+        className: 'JavierN',
+        classNameF: 'JavierNF',
+        BoleanoF: false,
 
     },
     {
-        FotoPerfil: FotoJohanMontas,
+        FotoPerfil: SpiderMan,
         UserName: 'MiguelA',
         Hour: '6h',
-        Publicacion: Paisaje,
-        Description: 'Hermoso Atardecer',
+        Publicacion: NoWayHome,
+        Description: 'DURISIMAAAAAAA LA PELICULA🔥🔥🔥',
         Number: '3',
+        className: 'MiguelA',
+        classNameF: 'MiguelAF',
+        BoleanoF: false,
 
     },
     {
-        FotoPerfil: FotoJohanMontas,
+        FotoPerfil: PixarAnimation,
         UserName: 'Batuce52',
         Hour: '6h',
-        Publicacion: Paisaje,
-        Description: 'Hermoso Atardecer',
+        Publicacion: GATO,
+        Description: 'Amo los gatos❤️❤️❤️',
         Number: '4',
+        className: 'Batuce52',
+        classNameF: 'Batuce52F',
+        BoleanoF: false,
 
     },
     
@@ -48,21 +67,4 @@ export default DataContents;
     //     Hour: '',
     //     Publicacion: '',
     //     Description: '',
-
-    // },
-    // {
-    //     FotoPerfil: '',
-    //     UserName: '',
-    //     Hour: '',
-    //     Publicacion: '',
-    //     Description: '',
-
-    // },
-    // {
-    //     FotoPerfil: '',
-    //     UserName: '',
-    //     Hour: '',
-    //     Publicacion: '',
-    //     Description: '',
-
-    // }
+//},
