c8ff2b9e50fa8af9cfe1c2004fe4d7de4346b9ef
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-07 21:08:24 -0400

    Se agrego un nuevo componente

---
+++ src/Home/components/Publication.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../style/Publication.less';
+
+export default function Publication() {
+  return (
+    <section className='PublicationContainer V5'>
+
+    </section>
+  )
+}
