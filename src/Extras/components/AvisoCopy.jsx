d9c1e3640faec4d4d40a1c1c553df45e5ee7b538
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-07 21:02:33 -0400

    Se creo un componente para avisar cuando se copia el link de una publicacion

---
+++ src/Extras/components/AvisoCopy.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../style/AvisoCopy.less';
+
+export default function AvisoCopy() {
+  return (
+    <dialog className='AvisoCopy' open>
+        Link copied to clipboard.
+    </dialog>
+  )
+};
