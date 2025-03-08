6708c32d8de73b692e319b8ee0d7c24d6254d042
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-07 21:10:16 -0400

    Se agregaron nuevos componentes

--- src/Structure.jsx
+++ src/Structure.jsx
@@ -7,6 +7,8 @@ import Explore from "./Explore/components/Explore";
 import Reels from "./Reel/components/Reels";
 import Message from "./Message/components/Message";
 import Modal_threePoint from "./Home/components/Modal_threePoint";
+import Publication from "./Home/components/Publication";
+// import AvisoCopy from "./Extras/components/AvisoCopy";
 
 
 export default function Structure() {
@@ -14,10 +16,12 @@ export default function Structure() {
     <section className="Structure">
         <Search/>
         <Modal_threePoint/>
+        {/* <AvisoCopy/> */}
         <HomeStructure/>
         <Explore/>
         <Reels/>
         <Message/>
+        <Publication/>  
     </section>
   )
 };
