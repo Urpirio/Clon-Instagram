b8c9dcd06dffaf42a2166e47cb188f89fe4faaca
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-07 21:06:26 -0400

    Se le agrego un valor de emergencia

--- src/Home/components/func/Modalthree.js
+++ src/Home/components/func/Modalthree.js
@@ -1,10 +1,14 @@
 import RestarMenu from "./RestarMenu";
+import { UserClickG } from "./GotoPost";
 
 
 export let UserClick;
 let Show;
 const ModalThree = (UserClickN) => {
     UserClick = UserClickN;
+    if(UserClick == undefined){
+        UserClick = UserClickG;
+    };
 
     if(Show == undefined){
         document.querySelector('.Modal_ThreeP').style.display = 'flex';
