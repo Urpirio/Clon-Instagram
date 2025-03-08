91338c1c24dd5e1aea45dccc88b931da31ce70a3
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-07 20:58:34 -0400

    Se eliminaron todas las funciones internas para convertirse en externas

--- src/BarraNavegacion/Components/Bnavegacion.jsx
+++ src/BarraNavegacion/Components/Bnavegacion.jsx
@@ -1,73 +1,18 @@
 import React from 'react';
-// import { Component, useEffect } from 'react';
-// import { flushSync } from 'react-dom';
-import { useState } from 'react';
 import '../Style/Bnavegacion.less'
 import DataBtn from '../Data/DataBtn';
 import LogoInstagram from '../img/LogoInstagram.png';
 import FotoPerfil from '../img/Fotoperfil.png';
-
+import Ventana0 from './func/Ventana0';
+import Ventana1 from './func/Ventana1';
+import Ventana2 from './func/Ventana2';
+import Ventana3 from './func/Ventana3';
+import Ventana4 from './func/Ventana4';
+import Ventana5 from './func/Ventana5';
+import Ventana6 from './func/Ventana6';
+import Ventana7 from './func/Ventana7';
 
 export default function Bnavegacion() {
-
-    const [ShowSearch, SetshowSearch] = useState(true);
-
-    const ventana0 = () => {//Home
-       for(let x = 1 ; x < 5; x++){
-        if( x == 1){
-            document.querySelector(`.V${x}`).style.display = 'flex';
-        }else{
-            document.querySelector(`.V${x}`).style.display = 'none';
-        }
-        
-       };
-    };
-    const ventana1 = () => {//search
-
-        SetshowSearch(!ShowSearch);
-        ShowSearch ? document.querySelector('.Search__div').style.display = 'flex' :
-        document.querySelector('.Search__div').style.display = 'none';
-
-    };
-    const ventana2 = () => {//explore
-        for(let x = 1 ; x < 5; x++){
-            if( x == 2){
-                document.querySelector(`.V${x}`).style.display = 'flex';
-            }else{
-                document.querySelector(`.V${x}`).style.display = 'none';
-            }
-         };
-    };
-    const ventana3 = () => {//Reels
-        for(let x = 1 ; x < 5; x++){
-            if( x == 3){
-                document.querySelector(`.V${x}`).style.display = 'flex';
-            }else{
-                document.querySelector(`.V${x}`).style.display = 'none';
-            }
-            
-           };
-    };
-    const ventana4 = () => {//Messages
-        for(let x = 1 ; x < 5; x++){
-            if( x == 4){
-                document.querySelector(`.V${x}`).style.display = 'flex';
-            }else{
-                document.querySelector(`.V${x}`).style.display = 'none';
-            }
-            
-           };
-    };
-    const ventana5 = () => {//Notifications
-        
-    };
-    const ventana6 = () => {//Create
-        
-    };
-    const ventana7 = () => {//Dashboard
-
-    };
-
 
   return (
     <section className='Bnavegacion'>
@@ -75,35 +20,35 @@ export default function Bnavegacion() {
             <img  src={LogoInstagram} alt="Logo Instagram" />
         </div>
         <div className='Navegacion' id='Navegacion'>
-            <span onClick={ventana0} className="ventana0">{/*Home*/}
+            <span onClick={Ventana0} className="ventana0">{/*Home*/}
                 <i className='bx bxs-home' ></i>
                 <span>{DataBtn[0].Vname}</span>
             </span>
-            <span onClick={ventana1}  className="ventana1">{/*Search*/}
+            <span onClick={Ventana1}  className="ventana1">{/*Search*/}
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/></svg>
                 <span>{DataBtn[1].Vname}</span>
             </span>
-            <span onClick={ventana2} className="ventana2">{/*Explore*/}
+            <span onClick={Ventana2} className="ventana2">{/*Explore*/}
                 <i className='bx bx-compass'></i>
                 <span>{DataBtn[2].Vname}</span>
             </span>
-            <span onClick={ventana3} className="ventana3">{/*Reels*/}
+            <span onClick={Ventana3} className="ventana3">{/*Reels*/}
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-play-btn" viewBox="0 0 16 16"><path d="M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z"/><path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"/></svg>
                 <span>{DataBtn[3].Vname}</span>
             </span>
-            <span onClick={ventana4} className="ventana4">{/*Messages*/}
+            <span onClick={Ventana4} className="ventana4">{/*Messages*/}
                 <i className='bx bxl-messenger'></i>
                 <span>{DataBtn[4].Vname}</span>
             </span>
-            <span onClick={ventana5} className="ventana5">{/*Notifications*/}
+            <span onClick={Ventana5} className="ventana5">{/*Notifications*/}
                 <i className='bx bx-heart'></i>
                 <span>{DataBtn[5].Vname}</span>
             </span>
-            <span onClick={ventana6} className="ventana6">{/*Create*/}
+            <span onClick={Ventana6} className="ventana6">{/*Create*/}
                 <i className='bx bx-plus'></i>
                 <span>{DataBtn[6].Vname}</span>
             </span>
-            <span onClick={ventana7} className="ventana7">{/*Dashboard*/}
+            <span onClick={Ventana7} className="ventana7">{/*Dashboard*/}
                 <i className='bx bx-bar-chart-alt-2'></i>
                 <span>{DataBtn[7].Vname}</span>
             </span>
