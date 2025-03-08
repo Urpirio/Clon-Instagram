0b58a0b08bd71a460c37cd0c856f147bfe28b971
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-07 21:05:46 -0400

    Se termino la funcion Copylink

--- src/Home/components/func/Copy_Link.js
+++ src/Home/components/func/Copy_Link.js
@@ -1,4 +1,24 @@
-const Copy_link = () => {
+import ModalThree from "./Modalthree";
+
+
+
+const Copy_link = async () => {
+        const AvisoCopy =  document.querySelector('.AvisoCopy');
+
+            AvisoCopy.style.display = 'flex';
+            AvisoCopy.style.animation = 'AvisoCopy 2s';
+
+
+        setTimeout(()=>{
+            AvisoCopy.style.animation = 'AvisoCopy2 2s';
+            setTimeout(()=>{
+                AvisoCopy.style.display = 'none';
+            },2000)
+        },2000);
+
+        navigator.clipboard.writeText(window.location.href);// Esto sirbe para copiar el texto;
+        
+        ModalThree()
 
 };
 export default Copy_link;
