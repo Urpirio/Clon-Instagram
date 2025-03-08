8bfb0407ab17ff0a484aa0ed27cbd1a13a3ecf93
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-07 21:00:24 -0400

    Se agrego la funcion ventana4 (Messages)

---
+++ src/BarraNavegacion/Components/func/Ventana4.js
@@ -0,0 +1,17 @@
+import DataContents from "../../../Home/Data/DataContents";
+
+const Ventana4 = () => {//Messages
+    for(let x = 1 ; x < 5; x++){
+        if( x == 4){
+            document.querySelector(`.V${x}`).style.display = 'flex';
+        }else{
+            document.querySelector(`.V${x}`).style.display = 'none';
+        }
+        
+    };
+    const LargoArray = DataContents.length;
+    for(let x = 0; x < LargoArray; x++){
+         DataContents[x].BoleanoG = false;
+    };
+};
+export default Ventana4
