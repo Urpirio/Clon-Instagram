c1f58ccfd539bd2368627f745ab11908f4a82bf3
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-07 21:03:09 -0400

    Se creo una estructura mayor

---
+++ src/Gstructure.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Structure from './Structure';
+import AvisoCopy from './Extras/components/AvisoCopy';
+
+export default function Gstructure() {
+  return (
+    <section className='Gstructure'>
+        <Structure/>
+        <AvisoCopy/>
+    </section>
+  )
+}
