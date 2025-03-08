711f8756191a2f7dda755110f724f1d25ee95492
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-07 20:59:31 -0400

    Se agrego la funcion ventana0(HOME)

---
+++ src/BarraNavegacion/Components/func/Ventana0.js
@@ -0,0 +1,18 @@
+import DataContents from "../../../Home/Data/DataContents";
+
+const Ventana0 = () => {//Home
+    for(let x = 1 ; x < 6; x++){
+     if( x == 1){
+         document.querySelector(`.V${x}`).style.display = 'flex';
+     }else{
+         document.querySelector(`.V${x}`).style.display = 'none';
+     }; 
+    };
+
+    const LargoArray = DataContents.length;
+    for(let x = 0; x < LargoArray; x++){
+         DataContents[x].BoleanoG = false;
+    };
+
+ };
+ export default Ventana0;
