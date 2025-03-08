92fa2e36bf43c246e62f2b9b498869f1c1ec34f9
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-07 21:00:09 -0400

    Se agrego la funcion ventana3 (Reels)

---
+++ src/BarraNavegacion/Components/func/Ventana3.js
@@ -0,0 +1,16 @@
+const Ventana3 = () => {//Reels
+    for(let x = 1 ; x < 5; x++){
+        if( x == 3){
+            document.querySelector(`.V${x}`).style.display = 'flex';
+        }else{
+            document.querySelector(`.V${x}`).style.display = 'none';
+        }
+        
+    };
+    
+    const LargoArray = DataContents.length;
+    for(let x = 0; x < LargoArray; x++){
+         DataContents[x].BoleanoG = false;
+    };
+};
+export default Ventana3;
