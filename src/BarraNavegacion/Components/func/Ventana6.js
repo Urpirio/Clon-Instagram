ff193a70b4e56fced75af6ec2c4f61aa0d744004
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-07 21:01:07 -0400

    Se agrego la funcion ventana6 (Create)

---
+++ src/BarraNavegacion/Components/func/Ventana6.js
@@ -0,0 +1,7 @@
+const Ventana6 = () => {//Create
+    const LargoArray = DataContents.length;
+    for(let x = 0; x < LargoArray; x++){
+         DataContents[x].BoleanoG = false;
+    };
+};
+export default Ventana6;
