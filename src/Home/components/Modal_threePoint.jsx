6226a9d02becc10ce6149e80a84bf87479ede518
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-08 21:31:28 -0400

    Se importaron las funciones about this account y embed

--- src/Home/components/Modal_threePoint.jsx
+++ src/Home/components/Modal_threePoint.jsx
@@ -6,6 +6,8 @@ import Add_to_Favorites from './func/AddtoFavorites';
 import GotoPost from './func/GotoPost';
 import Shareto from './func/ShareTo';
 import Copy_link from './func/Copy_Link';
+import Embed from './func/Embed';
+import About_This_Account from './func/About_This_Account';
 import Cancel from './func/Cancel';
 
 
@@ -24,8 +26,8 @@ export default function Modal_threePoint() {
            <span onClick={GotoPost} className="GSpan S4">Go to post</span>
            <span onClick={Shareto} className="GSpan S5">Share to...</span>
            <span onClick={Copy_link} className="GSpan S6">Copy link</span>
-           <span className="GSpan S7">Embed</span>
-           <span className="GSpan S8">About this account</span>
+           <span onClick={Embed} className="GSpan S7">Embed</span>
+           <span onClick={About_This_Account} className="GSpan S8">About this account</span>
            <span onClick={Cancel} className="GSpan bottom S9">Cancel</span>
         </div>
     </dialog>
