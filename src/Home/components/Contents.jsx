1c7a3aecb21fe96c845663e4b8f6b7f4162b1427
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-08 21:30:05 -0400

    Se agrego una nueva propiedad.

--- src/Home/components/Contents.jsx
+++ src/Home/components/Contents.jsx
@@ -18,6 +18,7 @@ export default function Contents() {
             Number = {DC.Number}
             className = {DC.className}
             classNameF =  {DC.classNameF}
+            ClassModalP = {DC.ClassModalP}
             />
         )
     })
