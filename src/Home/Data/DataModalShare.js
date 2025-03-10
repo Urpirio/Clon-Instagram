23d7b859bf9f7d6ec23a6387373a6526caaf025a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-09 20:23:35 -0400

    Se agrego un nuevo array de Data para ModalShare

---
+++ src/Home/Data/DataModalShare.js
@@ -0,0 +1,37 @@
+const DataModalShare = [
+    {
+        ModalPerfil: [
+            {
+                Icon: '',
+                Title: 'HHH'
+            },
+            {
+                Icon: '',
+                Title: ''
+            },
+            {
+                Icon: '',
+                Title: ''
+            },
+            {
+                Icon: '',
+                Title: ''
+            },
+            {
+                Icon: '',
+                Title: ''
+            },
+            {
+                Icon: '',
+                Title: ''
+            },
+            {
+                Icon: '',
+                Title: ''
+            },
+        
+
+        ]
+    }
+];
+export default DataModalShare;
