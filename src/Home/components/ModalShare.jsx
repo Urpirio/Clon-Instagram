b8a018811b7b9c24c1af958d6c37702567d710e5
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-09 20:21:50 -0400

    Se agrego un nuevo componente

---
+++ src/Home/components/ModalShare.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/ModalShare.less';
+
+export default function ModalShare() {
+  return (
+    <dialog className='ModalShare' open>
+        <div className='ModalShare__div1'>
+          hola
+        </div>
+    </dialog>
+  )
+}
