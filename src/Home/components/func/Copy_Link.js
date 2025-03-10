84d6e0bb562545764fd3fbd0788dd038816cbe1c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-10 17:49:56 -0400

    Se expandio el uso de esta funcion a otros componentes

--- src/Home/components/func/Copy_Link.js
+++ src/Home/components/func/Copy_Link.js
@@ -2,23 +2,44 @@ import ModalThree from "./Modalthree";
 
 
 
-const Copy_link = async () => {
-        const AvisoCopy =  document.querySelector('.AvisoCopy');
+const Copy_link = async (click) => {
+        if(click == undefined){
+            const AvisoCopy =  document.querySelector('.AvisoCopy');
 
             AvisoCopy.style.display = 'flex';
             AvisoCopy.style.animation = 'AvisoCopy 2s';
 
 
-        setTimeout(()=>{
-            AvisoCopy.style.animation = 'AvisoCopy2 2s';
             setTimeout(()=>{
-                AvisoCopy.style.display = 'none';
-            },2000)
-        },2000);
+                AvisoCopy.style.animation = 'AvisoCopy2 2s';
+                setTimeout(()=>{
+                    AvisoCopy.style.display = 'none';
+                },2000)
+            },2000);
 
-        navigator.clipboard.writeText(window.location.href);// Esto sirbe para copiar el texto;
+            navigator.clipboard.writeText(window.location.href);// Esto sirbe para copiar el texto;
         
-        ModalThree()
+            ModalThree()
+        }else{
+            click = undefined;
+            document.querySelector('.ModalShare').style.display = 'none';
+            document.querySelector('.ModalShare__div1').innerHTML = '';
+
+            const AvisoCopy =  document.querySelector('.AvisoCopy');
+
+            AvisoCopy.style.display = 'flex';
+            AvisoCopy.style.animation = 'AvisoCopy 2s';
+
+
+            setTimeout(()=>{
+                AvisoCopy.style.animation = 'AvisoCopy2 2s';
+                setTimeout(()=>{
+                    AvisoCopy.style.display = 'none';
+                },2000)
+            },2000);
+
+            navigator.clipboard.writeText(window.location.href)
+        };
 
 };
 export default Copy_link;
