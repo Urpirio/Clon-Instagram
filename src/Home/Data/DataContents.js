a42819dd11d62b9dac5149f2ce9ac5f5f8bd0f52
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-10 17:53:37 -0400

    Se agregaron nuevas propiedades

--- src/Home/Data/DataContents.js
+++ src/Home/Data/DataContents.js
@@ -2,11 +2,12 @@ import ChicoNike from '../Img/DataContent_Img/FotoPerfil/ChicoNike.png';
 import HombreR from '../Img/DataContent_Img/FotoPerfil/HombreR.png';
 import PixarAnimation from '../Img/DataContent_Img/FotoPerfil/PixarAnimation.png';
 import SpiderMan  from '../Img/DataContent_Img/FotoPerfil/Spiderman.png';
+import CocoFoto from '../Img/DataContent_Img/FotoPerfil/CocoFoto.png';
 import Paisaje from '../Img/DataContent_Img/FotoPubliciones/Paisaje.png'
 import NoWayHome from '../Img/DataContent_Img/FotoPubliciones/NowayHome.png';
 import Codejs from '../Img/DataContent_Img/FotoPubliciones/CodesJs.png';
 import GATO from '../Img/DataContent_Img/FotoPubliciones/GATO.png';
-
+import GatoNegro from '../Img/DataContent_Img/FotoPubliciones/GAtoNegro.png';
 
 
 
@@ -24,8 +25,13 @@ const DataContents = [
         BoleanoFavorito: false,
         BoleanoGotopost: false,
         BoleanoModalPerfil: false,
+        BoleanoSendMessage: false,
         BoleanoFollow: true,
         Verificado: false,
+        Like: 'Like',
+        BoleanoLike: false,
+        SaveId: 'Save',
+        // SaveClass: 'bx bx-bookmark',
         FechaV: '',
         Country: 'Estados Unidos',
         FechaSignIn: 'November 2022',
@@ -45,8 +51,13 @@ const DataContents = [
         BoleanoFavorito: false,
         BoleanoGotopost: false,
         BoleanoModalPerfil: false,
+        BoleanoSendMessage: false,
         BoleanoFollow: true,
         Verificado: false,
+        Like: 'Like1',
+        BoleanoLike: false,
+        SaveId: 'Save1',
+        SaveClass: 'bx bx-bookmark',
         FechaV: '',
         Country: 'Canada',
         FechaSignIn: 'December 2024',
@@ -66,8 +77,14 @@ const DataContents = [
         BoleanoFavorito: false,
         BoleanoGotopost: false,
         BoleanoModalPerfil: false,
+        BoleanoSendMessage: false,
         BoleanoFollow: true,
         Verificado: true,
+        Like: 'Like2',
+        BoleanoLike: false,
+        SaveId: 'Save2',
+        SaveClass: 'bx bx-bookmark',
+        BoleanoSave: false,
         FechaV: 'November 2020',
         Country: 'Republica Dominicana',
         FechaSignIn: 'July 2018',
@@ -88,13 +105,46 @@ const DataContents = [
         BoleanoFavorito: false,
         BoleanoGotopost: false,
         BoleanoModalPerfil: false,
+        BoleanoSendMessage: false,
+        BoleanoSearch: false,
         BoleanoFollow: true,
         Verificado: true,
+        Like: 'Like3',
+        BoleanoLike: false,
+        SaveId: 'Save3',
+        SaveClass: 'bx bx-bookmark',
         FechaV: 'December 2024',
         Country: 'Francia',
         FechaSignIn: 'June 2014',
         ClassModalP: 'Batuce52P',
         ClassModalPR: 'Batuce52PR',
+
+       
+    },
+    {
+        FotoPerfil: CocoFoto,
+        UserName: 'NaALA',
+        Hour: '6h',
+        Publicacion: GatoNegro,
+        Description: 'Amo los gatos❤️❤️❤️',
+        Number: '5',
+        className: 'NaALA',
+        classNameF: 'NaALAF',
+        BoleanoFavorito: false,
+        BoleanoGotopost: false,
+        BoleanoModalPerfil: false,
+        BoleanoSendMessage: false,
+        BoleanoFollow: true,
+        Verificado: true,
+        Like: 'Like4',
+        BoleanoLike: false,
+        SaveId: 'Save4',
+        SaveClass: 'bx bx-bookmark',
+        FechaV: 'December 2024',
+        Country: 'Haiti',
+        FechaSignIn: 'June 2014',
+        ClassModalP: 'NaALAP',
+        ClassModalPR: 'NaALAPR',
 
        
     },
