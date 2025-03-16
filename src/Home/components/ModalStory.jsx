f38329ff239ee01ec514ddb348829bc1274bdb45
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-15 21:26:33 -0400

    Se agrego un nuevo componente

---
+++ src/Home/components/ModalStory.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../style/ModalStory.less';
+
+export default function ModalStory() {
+  return (
+    <section className='ModalStory'>
+        <div className='ModalStory_div1'>
+            
+        </div>
+        <div className='ModalStory_div2'>
+           
+        </div>
+        <div className='ModalStory_div3'>
+            
+        </div>
+    </section>
+  )
+}
